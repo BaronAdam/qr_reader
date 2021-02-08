a33dc2a9eca83c23839bd89d7b776ff58d1b2273
Adam <adamo.b1997@gmail.com>
2021-02-08 13:51:24 +0100

    Convert IconTextButton to StatefulWidget

--- lib/widgets/icon_text_button.dart
+++ lib/widgets/icon_text_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qr_reader/widgets/formatted_text.dart';
 
-class IconTextButton extends StatelessWidget {
+class IconTextButton extends StatefulWidget {
   final onPressed;
   final String text;
   final Icon icon;
@@ -16,18 +16,23 @@ class IconTextButton extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _IconTextButtonState createState() => _IconTextButtonState();
+}
+
+class _IconTextButtonState extends State<IconTextButton> {
+  @override
   Widget build(BuildContext context) {
     return FlatButton(
-      onPressed: null,
-      minWidth: minWidth,
+      onPressed: widget.onPressed,
+      minWidth: widget.minWidth,
       child: Row(
         children: [
-          icon,
+          widget.icon,
           SizedBox(
             width: 10,
           ),
           FormattedText(
-            text: text,
+            text: widget.text,
             fontSize: 20,
           ),
         ],
